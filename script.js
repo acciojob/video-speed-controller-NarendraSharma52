aa9fad11712ae04c9682c9257297f902ec2adb38
Narendra Sharma <154484787+NarendraSharma52@users.noreply.github.com>
2026-02-05 12:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 const video = document.getElementById("video");
 const toggleButton = document.getElementById("toggle");
 const progressBar = document.getElementById("progress");
-const progressFilled = document.getElementById("progress__filled");
 const volumeController = document.getElementById("volume");
-const playBackSpeedController = document.getElementById("playbackSpeed");
+const playbackSpeedController = document.getElementById("playbackSpeed");
 const rewindButton = document.getElementById("rewind");
 const skipButton = document.getElementById("skip");
 
-toggleButton.addEventListener('click' , () => {
-    if(video.paused) {
+/* Play / Pause */
+toggleButton.addEventListener("click", () => {
+    if (video.paused) {
         video.play();
-        toggleButton.innerText = " ❚ ❚";
-    }
-    else {
+        toggleButton.innerText = "❚ ❚";
+    } else {
         video.pause();
         toggleButton.innerText = "►";
     }
-})
+});
 
-video.addEventListener("timeUpdate", () => {
+/* Update progress bar */
+video.addEventListener("timeupdate", () => {
     const progress = (video.currentTime / video.duration) * 100;
     progressBar.value = progress;
-    progressFilled.style.width = `${progress}%`;
-    console.log(progressFilled);
-})
+});
 
+/* Scrub video */
 progressBar.addEventListener("input", () => {
-    const value = progressBar.value;
-    video.currentTime = (value/100) * video.duration;
-    console.log(video.currentTime);
-})
+    video.currentTime = (progressBar.value / 100) * video.duration;
+});
 
+/* Volume control */
 volumeController.addEventListener("input", () => {
     video.volume = volumeController.value;
-    console.log(video.volume);
-})
+});
 
-playBackSpeedController.addEventListener("click", () => {
-    video.playbackRate = Number(playBackSpeedController.value);
-    console.dir(video);
-    console.log(video.playbackRate);
-})
+/* Playback speed */
+playbackSpeedController.addEventListener("input", () => {
+    video.playbackRate = playbackSpeedController.value;
+});
 
+/* Rewind 10 seconds */
 rewindButton.addEventListener("click", () => {
     video.currentTime -= 10;
-})
+});
 
+/* Skip 25 seconds */
 skipButton.addEventListener("click", () => {
     video.currentTime += 25;
-})
+});
