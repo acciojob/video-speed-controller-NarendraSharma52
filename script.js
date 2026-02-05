ca4f61806c63b6887846bc9b67739501f9a1a917
Narendra Sharma <154484787+NarendraSharma52@users.noreply.github.com>
2026-02-05 10:31:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.getElementById("video");
+const toggleButton = document.getElementById("toggle");
+const progressBar = document.getElementById("progress");
+const progressFilled = document.getElementById("progress__filled");
+const volumeController = document.getElementById("volume");
+const playBackSpeedController = document.getElementById("playbackSpeed");
+const rewindButton = document.getElementById("rewind");
+const skipButton = document.getElementById("skip");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+toggleButton.addEventListener('click' , () => {
+    if(video.paused) {
+        video.play();
+        toggleButton.innerText = " ❚ ❚";
     }
+    else {
+        video.pause();
+        toggleButton.innerText = "►";
+    }
+})
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+video.addEventListener("timeUpdate", () => {
+    const progress = (video.currentTime / video.duration) * 100;
+    progressBar.value = progress;
+    progressFilled.style.width = `${progress}%`;
+    console.log(progressFilled);
+})
+
+progressBar.addEventListener("input", () => {
+    const value = progressBar.value;
+    video.currentTime = (value/100) * video.duration;
+    console.log(video.currentTime);
+})
+
+volumeController.addEventListener("input", () => {
+    video.volume = volumeController.value;
+    console.log(video.volume);
+})
+
+playBackSpeedController.addEventListener("click", () => {
+    video.playbackRate = Number(playBackSpeedController.value);
+    console.dir(video);
+    console.log(video.playbackRate);
+})
+
+rewindButton.addEventListener("click", () => {
+    video.currentTime -= 10;
+})
+
+skipButton.addEventListener("click", () => {
+    video.currentTime += 25;
+})
